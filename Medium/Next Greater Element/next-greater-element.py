938769244bc53a9ec9cc007d72207a2f975b6bed
Seetharaman <seetharamanshan699@gmail.com>
2023-02-23 11:10:11 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Next Greater Element/next-greater-element.py
@@ -0,0 +1,48 @@
+#User function Template for python3
+
+
+class Solution:
+    def nextLargerElement(self,arr,n):
+        ans = [-1 for i in range(n)]
+        #print(ans)
+        stack = []
+        for j in range(n-1, -1, -1):
+            #print(j)
+            v = arr[j]
+            while len(stack) and v >= stack[-1]:
+                stack.pop()
+            if len(stack):
+                ans[j] = stack[-1]
+            stack.append(v)
+        return ans
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+
+        a = list(map(int,input().strip().split()))
+        obj = Solution()
+        res = obj.nextLargerElement(a,n)
+        for i in range (len (res)):
+            print (res[i], end = " ")
+        print ()
+# } Driver Code Ends
