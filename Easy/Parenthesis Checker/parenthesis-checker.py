6375675997f8b098d9b777ae7844a54212b22a90
Seetharaman <seetharamanshan699@gmail.com>
2023-02-23 11:25:00 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Parenthesis Checker/parenthesis-checker.py
@@ -0,0 +1,61 @@
+#User function Template for python3
+
+class Solution:
+    #Function to check if brackets are balanced or not.
+    def ispar(self,x):
+        op = tuple('({[')
+        cp = tuple(')}]')
+        m = dict(zip(op, cp))
+        queue = []
+        for i in x:
+            #print("i in x: ",i)
+            if i in op:
+                #print("i in op: ",i)
+                queue.append(m[i])
+                #print("queue.append(m[i]): ",queue)
+            elif i in cp:
+                if not queue or i != queue.pop():
+                    return False
+        if not queue:
+            return True
+        else:
+            return False
+
+                
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        #n = int(input())
+        #n,k = map(int,imput().strip().split())
+        #a = list(map(int,input().strip().split()))
+        s = str(input())
+        obj = Solution()
+        if obj.ispar(s):
+            print("balanced")
+        else:
+            print("not balanced")
+# } Driver Code Ends
